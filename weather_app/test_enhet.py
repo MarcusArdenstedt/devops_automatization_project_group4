ec5549cf754977feee136246008a09a20686b3ba
Marcus <marcus.ericsson_92@icloud.com>
2025-11-11 20:42:58 +0100

    small changing on unit test

--- weather_app/test_enhet.py
+++ weather_app/test_enhet.py
@@ -1,9 +1,8 @@
 from extract_data import create_dict
 from unittest.mock import patch
-import pandas as pd
 
-# fajkdata to run a unitetest for the function.
-def test_length_return_24():
+# fakedata to run a unit test for the function.
+def test_length():
     """Create fajk data to do unitetest. it will simulate what the api request get.
     This will make sure that api request is not use when running the test"""
     fake_data = {
@@ -12,13 +11,13 @@ def test_length_return_24():
                 "validTime": f"2025-11-10T{i:02d}:00:00Z",
                 "parameters": [{"name": "t", "values": [i]}]
             }
-            for i in range(30)
+            for i in range(24)
         ]
     }
 
     with patch("extract_data.get_weather_data", return_value=fake_data):
+        
         df = create_dict("stockholm")
         
-        assert isinstance(df, pd.DataFrame)
         assert len(df) == 24
         assert list(df.columns) == ["time", "degree"]
